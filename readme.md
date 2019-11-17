0fcbc13b88cc6cc247e09718887510c479440f05
Doğukan Öksüz <me@dogukan.dev>
2019-11-17 21:34:46 +0300

    Init project, installed bootstrap, ui elements etc.

---
+++ readme.md
@@ -0,0 +1,4 @@
+# DBMS Project
+Homework of DBMS lesson.
+
+Mezun sosyal ağı.
---
+++ resources/js/app.js
@@ -0,0 +1,2 @@
+require('../../node_modules/bootstrap/dist/js/bootstrap');
+require('../../node_modules/shards-ui/dist/js/shards.min');
